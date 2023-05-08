ff762e68155eeca449847e948450ca6b0973cdc0
Evans Wanjau <evanswanjau@gmail.com>
2023-05-08 15:18:13 +0300

    fix: refactored code to use AddButton component

--- src/layouts/pageLayout.js
+++ src/layouts/pageLayout.js
@@ -2,8 +2,9 @@ import React from "react";
 import SideNav from "../components/sideNav";
 import { Empty } from "../components/empty";
 import { Loader } from "../components/loader";
+import { AddButton } from "../components/addButton";
 
-const PageLayout = ({ length, loading, content }) => {
+const PageLayout = ({ length, loading, content, addButton, action = null }) => {
     if (length < 1) content = <Empty />;
     if (loading) content = <Loader />;
 
@@ -12,7 +13,10 @@ const PageLayout = ({ length, loading, content }) => {
             <div className="w-2/12">
                 <SideNav />
             </div>
-            <div className="w-10/12 p-5 mt-14">{content}</div>
+            <div className="w-10/12 p-10 mt-12 h-[calc(100vh-3em)] overflow-scroll">
+                {content}
+                {addButton && <AddButton action={action} />}
+            </div>
         </div>
     );
 };
