8ba91d3bbd390762620b5acd77b03f00e9f1028a
Evans Wanjau <evanswanjau@gmail.com>
2023-05-12 18:42:49 +0300

    fix: refactred pageLayout to accomodate all pages

--- src/layouts/pageLayout.js
+++ src/layouts/pageLayout.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSnackbar } from "notistack";
 import SideNav from "../components/sideNav";
-import { Empty } from "../components/empty";
-import { Loader } from "../components/loader";
 import { AddButton } from "../components/addButton";
+import { apiRequest } from "../api/api-calls";
+import { Loader } from "../components/loader";
+import { Empty } from "../components/empty";
+import { getForm } from "../helpers/forms";
 
-const PageLayout = ({ length, loading, content, addButton, action = null }) => {
-    if (length < 1) content = <Empty />;
-    if (loading) content = <Loader />;
+const PageLayout = ({ url, pageContent, addButton, form }) => {
+    const [data, setData] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [revealForm, setRevealForm] = useState(false);
+    const { enqueueSnackbar } = useSnackbar();
+
+    const getData = () => {
+        apiRequest("get", url)
+            .then(({ data }) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch(({ message }) => {
+                enqueueSnackbar(message, {
+                    variant: "error",
+                    anchorOrigin: { vertical: "top", horizontal: "center" },
+                });
+            });
+    };
+
+    const getContent = () => {
+        if (loading) return <Loader />;
+
+        if (revealForm) {
+            addButton = false;
+            return getForm(url, setRevealForm);
+        }
+
+        if (!revealForm && data) {
+            if (data.length < 1) return <Empty />;
+            if (data.length > 1) return pageContent(data);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []); // eslint-disable-line
 
     return (
         <div className="flex">
@@ -14,8 +51,10 @@ const PageLayout = ({ length, loading, content, addButton, action = null }) => {
                 <SideNav />
             </div>
             <div className="w-10/12 p-10 mt-12 h-[calc(100vh-3em)] overflow-scroll">
-                {content}
-                {addButton && <AddButton action={action} />}
+                {getContent()}
+                {!loading && addButton && (
+                    <AddButton setRevealForm={setRevealForm} />
+                )}
             </div>
         </div>
     );
