139996e7d8f101f923b388a2b342bcb340a61023
Evans Wanjau <evanswanjau@gmail.com>
2023-05-12 18:37:22 +0300

    new: added apiRequest helper

---
+++ src/api/api-calls.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const apiRequest = (method, url, data = {}) => {
+    return axios({
+        method: method,
+        url: process.env.REACT_APP_API_URL + url,
+        data: data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
