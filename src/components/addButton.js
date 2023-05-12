af7b0215f27d5cc3ea2b7b90802f098c40c1372f
Evans Wanjau <evanswanjau@gmail.com>
2023-05-12 18:41:52 +0300

    update: refactored add button action function

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -1,12 +1,12 @@
 import { HiPlus } from "react-icons/hi2";
 
-export const AddButton = ({ action }) => {
+export const AddButton = ({ setRevealForm }) => {
     return (
         <div className="fixed bottom-5 right-5">
             <button
-                className="bg-red-800 rounded-full p-3 shadow-lg"
+                className="bg-[#ED7423] rounded-full p-3 shadow-lg"
                 onClick={() => {
-                    action();
+                    setRevealForm(true);
                 }}
             >
                 <HiPlus className="text-3xl text-white" />
