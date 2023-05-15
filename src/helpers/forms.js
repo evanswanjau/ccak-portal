e816e55f142557a7e90a2269a8bfb3f342fa5913
Evans Wanjau <evanswanjau@gmail.com>
2023-05-16 01:44:11 +0300

    fix: added getData function parameter

--- src/helpers/forms.js
+++ src/helpers/forms.js
@@ -1,6 +1,8 @@
 import { PostForm } from "../layouts/forms/post-form";
 
-export const getForm = (url, setRevealForm, id) => {
+export const getForm = (url, setRevealForm, id, getData) => {
     if (url === "posts")
-        return <PostForm setRevealForm={setRevealForm} id={id} />;
+        return (
+            <PostForm setRevealForm={setRevealForm} id={id} getData={getData} />
+        );
 };
