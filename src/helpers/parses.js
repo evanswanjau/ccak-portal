4be28dc1a023152c1ba496074b84002b1987071e
Evans Wanjau <evanswanjau@gmail.com>
2023-07-10 09:30:42 +0300

    Commented out parsing code

--- src/helpers/parses.js
+++ src/helpers/parses.js
@@ -1,4 +1,4 @@
-import { RiQuillPenFill, RiSendPlaneFill } from "react-icons/ri";
+// import { RiQuillPenFill, RiSendPlaneFill } from "react-icons/ri";
 
 export const parseData = (url, data) => {
     if (url === "post") {
@@ -11,51 +11,51 @@ export const parseData = (url, data) => {
                 );
             }
 
-            if (item.category) {
-                item.category = (
-                    <p className="capitalize">
-                        {item.category.replace(/-/g, " ")}
-                    </p>
-                );
-            }
+            // if (item.category) {
+            //     item.category = (
+            //         <p className="capitalize">
+            //             {item.category.replace(/-/g, " ")}
+            //         </p>
+            //     );
+            // }
 
-            if (item.access) {
-                let color =
-                    item.access === "public"
-                        ? "bg-emerald-300"
-                        : item.access === "private"
-                        ? "bg-red-200"
-                        : "bg-gray-200";
-                item.access = (
-                    <p
-                        className={`capitalize ${color} text-grey-600 pt-[0.2em] pb-[0.3em] px-1 rounded-lg text-center`}
-                    >
-                        {item.access}
-                    </p>
-                );
-            }
+            // if (item.access) {
+            //     let color =
+            //         item.access === "public"
+            //             ? "bg-emerald-300"
+            //             : item.access === "private"
+            //             ? "bg-red-200"
+            //             : "bg-gray-200";
+            //     item.access = (
+            //         <p
+            //             className={`capitalize ${color} text-grey-600 pt-[0.2em] pb-[0.3em] px-1 rounded-lg text-center`}
+            //         >
+            //             {item.access}
+            //         </p>
+            //     );
+            // }
 
-            if (item.status) {
-                let color =
-                    item.status === "draft"
-                        ? "bg-gray-400 w-fit mx-auto"
-                        : "bg-teal-600 w-fit mx-auto";
+            // if (item.status) {
+            //     let color =
+            //         item.status === "draft"
+            //             ? "bg-gray-400 w-fit mx-auto"
+            //             : "bg-teal-600 w-fit mx-auto";
 
-                let icon =
-                    item.status === "draft" ? (
-                        <RiQuillPenFill className="text-sm ml-2 mt-1" />
-                    ) : (
-                        <RiSendPlaneFill className="text-sm ml-2 mt-1" />
-                    );
+            //     let icon =
+            //         item.status === "draft" ? (
+            //             <RiQuillPenFill className="text-sm ml-2 mt-1" />
+            //         ) : (
+            //             <RiSendPlaneFill className="text-sm ml-2 mt-1" />
+            //         );
 
-                item.status = (
-                    <p
-                        className={`flex justify-center capitalize ${color} text-white py-1 px-3 rounded-full text-center`}
-                    >
-                        {item.status} {icon}
-                    </p>
-                );
-            }
+            //     item.status = (
+            //         <p
+            //             className={`flex justify-center capitalize ${color} text-white py-1 px-3 rounded-full text-center`}
+            //         >
+            //             {item.status} {icon}
+            //         </p>
+            //     );
+            // }
 
             return item;
         });
