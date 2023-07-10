61f1ce4eadad84be4b75caed30e1f4af6ab1a871
Evans Wanjau <evanswanjau@gmail.com>
2023-07-10 09:26:24 +0300

    Removed incomplete links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Dashboard from "./pages/dashboard";
-
 import TopNav from "./components/topNav";
-// import Payments from "./pages/payments";
 import Posts from "./pages/posts";
-import News from "./pages/pages/media-centre/news";
-// import PressReleases from "./pages/pages/media-centre/press-releases";
 
 const App = () => {
     return (
@@ -15,15 +10,7 @@ const App = () => {
             <TopNav />
 
             <Switch>
-                <Route exact path="/pages/media-centre/news" component={News} />
-                {/* <Route
-                    exact
-                    path="/pages/media-centre/press-releases"
-                    component={PressReleases}
-                /> */}
                 <Route exact path="/posts" component={Posts} />
-                {/* <Route exact path="/payments" component={Payments} /> */}
-                <Route exact path="/" component={Dashboard} />
             </Switch>
         </Router>
     );
