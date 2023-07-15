ffbeb35b73648b93a9a1e0567aa5bbc120ad5b98
Evans Wanjau <evanswanjau@gmail.com>
2023-07-15 18:28:38 +0300

    update: added payment form in form query

--- src/helpers/forms.js
+++ src/helpers/forms.js
@@ -1,8 +1,17 @@
+import { PaymentForm } from "../layouts/forms/payment-form";
 import { PostForm } from "../layouts/forms/post-form";
 
 export const getForm = (page, setRevealForm, id, getData) => {
     if (page === "posts")
         return (
             <PostForm setRevealForm={setRevealForm} id={id} getData={getData} />
+        );
+    if (page === "payments")
+        return (
+            <PaymentForm
+                setRevealForm={setRevealForm}
+                id={id}
+                getData={getData}
+            />
         );
 };
