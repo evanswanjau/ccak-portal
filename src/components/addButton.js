a020f9390eb89ec4c8211b985f176f8b9f599386
Evans Wanjau <evanswanjau@gmail.com>
2023-07-17 15:58:13 +0300

    fix: added setID to reset ID

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -1,12 +1,13 @@
 import { HiPlus } from "react-icons/hi2";
 
-export const AddButton = ({ setRevealForm }) => {
+export const AddButton = ({ setRevealForm, setID }) => {
     return (
         <div className="fixed bottom-10 right-10 flex flex-col place-items-end">
             <button
                 className="bg-[#ED7423] rounded-full p-3 shadow-lg"
                 onClick={() => {
                     setRevealForm(true);
+                    setID(null);
                 }}
             >
                 <HiPlus className="text-3xl text-white" />
