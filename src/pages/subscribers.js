b58ac3fba43c083eb1433d6bb19f62507699f97d
Evans Wanjau <evanswanjau@gmail.com>
2023-07-18 13:16:53 +0300

    update: removed subscribers url prop

--- src/pages/subscribers.js
+++ src/pages/subscribers.js
@@ -1,5 +1,5 @@
 import { PageLayout } from "../layouts/pageLayout";
 
 export const SubscribersPage = () => {
-    return <PageLayout page="subscribers" url="subscriber" addButton={false} />;
+    return <PageLayout page="subscribers" addButton={false} />;
 };
