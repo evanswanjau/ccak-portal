2c00cde915fd2eb04e16fc49adab189f5c2605f5
Evans Wanjau <evanswanjau@gmail.com>
2023-07-19 11:04:17 +0300

    update: added invoice filter menu

--- src/components/forms/filter.js
+++ src/components/forms/filter.js
@@ -124,18 +124,31 @@ export const FilterForm = ({ page, search, updateSearch }) => {
             </div>
         );
     }
+
     if (page === "payments") {
         return (
             <div className="py-3 flex justify-end space-x-2">
                 <input
                     className="w-3/12 text-sm block text-gray-700 border border-gray-200 rounded-lg py-2 px-3 leading-tight focus:outline-none focus:border-gray-500"
                     type="text"
-                    placeholder="Search payments"
-                    value={search.keyword}
+                    placeholder="Search transaction ID"
+                    value={search.transaction_id}
                     onChange={(event) => {
                         updateSearch({
                             ...search,
-                            keyword: event.target.value,
+                            transaction_id: event.target.value,
+                        });
+                    }}
+                />
+                <input
+                    className="w-3/12 text-sm block text-gray-700 border border-gray-200 rounded-lg py-2 px-3 leading-tight focus:outline-none focus:border-gray-500"
+                    type="text"
+                    placeholder="Search invoice number"
+                    value={search.invoice_id}
+                    onChange={(event) => {
+                        updateSearch({
+                            ...search,
+                            invoice_number: event.target.value,
                         });
                     }}
                 />
@@ -144,12 +157,74 @@ export const FilterForm = ({ page, search, updateSearch }) => {
                     onChange={(event) => {
                         updateSearch({
                             ...search,
-                            access: event.target.value,
+                            method: event.target.value,
                         });
                     }}
                 >
                     <option value={""}>Choose Method</option>
                     {["bank deposit", "mpesa", "cash"].map((item) => {
+                        return (
+                            <option
+                                key={item}
+                                value={item}
+                                selected={search.method === item}
+                            >
+                                {item.slice(0, 1).toUpperCase() +
+                                    item.substring(1)}
+                            </option>
+                        );
+                    })}
+                </select>
+                <button
+                    type="button"
+                    className="flex focus:outline-none bg-gray-100 text-teal-900 hover:bg-gray-200 font-medium rounded-lg text-sm px-3 pt-2 pb-[0.8em] transition duration-150 ease-in-out"
+                    onClick={() => {
+                        updateSearch({
+                            transaction_id: "",
+                            method: "",
+                            invoice_number: "",
+                            page: 1,
+                            limit: 100,
+                        });
+                    }}
+                >
+                    <RiCloseCircleFill className="text-sm mr-2 mt-1" />
+                    Reset
+                </button>
+            </div>
+        );
+    }
+
+    if (page === "invoices") {
+        return (
+            <div className="py-3 flex justify-end space-x-2">
+                <input
+                    className="w-3/12 text-sm block text-gray-700 border border-gray-200 rounded-lg py-2 px-3 leading-tight focus:outline-none focus:border-gray-500"
+                    type="text"
+                    placeholder="Search invoice number"
+                    value={search.invoice_number}
+                    onChange={(event) => {
+                        updateSearch({
+                            ...search,
+                            invoice_number: event.target.value,
+                        });
+                    }}
+                />
+                <select
+                    className="w-2/12 block text-sm text-gray-700 bg-white border border-gray-200 rounded-lg py-2 px-3 leading-tight focus:outline-none focus:border-gray-500"
+                    onChange={(event) => {
+                        updateSearch({
+                            ...search,
+                            type: event.target.value,
+                        });
+                    }}
+                >
+                    <option value={""}>Choose Type</option>
+                    {[
+                        "member registration",
+                        "annual subscription",
+                        "donation",
+                    ].map((item) => {
                         return (
                             <option
                                 key={item}
@@ -172,7 +247,7 @@ export const FilterForm = ({ page, search, updateSearch }) => {
                     }}
                 >
                     <option value={""}>Choose Type</option>
-                    {["subscription", "donation"].map((item) => {
+                    {["paid", "unpaid"].map((item) => {
                         return (
                             <option
                                 key={item}
@@ -190,16 +265,11 @@ export const FilterForm = ({ page, search, updateSearch }) => {
                     className="flex focus:outline-none bg-gray-100 text-teal-900 hover:bg-gray-200 font-medium rounded-lg text-sm px-3 pt-2 pb-[0.8em] transition duration-150 ease-in-out"
                     onClick={() => {
                         updateSearch({
-                            keyword: "",
-                            table: "posts",
-                            category: "",
-                            technology: "",
-                            project_status: "",
-                            access: "",
+                            invoice_number: "",
+                            type: "",
                             status: "",
                             page: 1,
                             limit: 10,
-                            ip_address: "",
                         });
                     }}
                 >
