a88645f05bff0019c1867369d733c9bd9b58954a
Evans Wanjau <evanswanjau@gmail.com>
2024-01-13 14:48:18 +0300

    Refactor form layout in post-form/published.js

--- src/layouts/forms/post-form/published.js
+++ src/layouts/forms/post-form/published.js
@@ -8,8 +8,8 @@ export const Published = ({
     setRevealForm,
 }) => {
     return (
-        <div className="flex justify-center h-[calc(100vh-14em)]">
-            <div className="text-center mt-10 w-9/12 h-fit">
+        <div className="flex items-center justify-center h-[calc(100vh-14em)] ">
+            <div className="text-center w-9/12 h-fit">
                 <h2 className="text-4xl text-gray-800">Congratulations</h2>
                 <h3 className="text-xl text-gray-400 my-5">
                     Your{" "}
@@ -31,18 +31,8 @@ export const Published = ({
                     type="button"
                     className="flex mx-auto my-10 text-white bg-teal-900 hover:bg-teal-950 font-medium rounded-lg text-sm px-4 pt-2 pb-[0.8em] transition duration-150 ease-in-out"
                     onClick={() => {
-                        updateData({
-                            ...data,
-                            status: "published",
-                        });
-                        submitData("post", "post/update/" + data.id, {
-                            ...data,
-                            step: "published",
-                            status: "published",
-                        }).then(() => {
-                            getData();
-                            setRevealForm(false);
-                        });
+                        getData();
+                        setRevealForm(false);
                     }}
                 >
                     Continue to posts
