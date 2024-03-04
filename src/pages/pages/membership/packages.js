6b4c34a0cc32ac1095fff54e01f51b6025f324e9
Evans Wanjau <evanswanjau@gmail.com>
2024-03-05 02:13:50 +0300

    Add PackagesSection to PackagesPage

--- src/pages/pages/membership/packages.js
+++ src/pages/pages/membership/packages.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import { HeaderForm } from "../../../layouts/forms/header-form";
 import { ContentPageLayout } from "../../../layouts/contentPageLayout";
+import { PackagesSection } from "../../../layouts/forms/content/membership/packages";
 
 export const PackagesPage = () => {
     const [data, updateData] = useState([]);
 
+    const content = (
+        <div className="space-y-8">
+            <HeaderForm data={data[0]} />
+            <PackagesSection data={data[1]} />
+        </div>
+    );
+
     return (
         <ContentPageLayout
             page="packages"
-            content={<HeaderForm data={data[0]} />}
+            content={content}
             data={data}
             updateData={updateData}
         />
